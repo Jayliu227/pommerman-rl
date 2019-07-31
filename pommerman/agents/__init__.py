5fbdad509322784c15830ccc18591facbe4f70e9
jingyuliu <jl7895@nyu.edu>
2019-07-31 23:57:44 +0800

    Added imports in agent __init__.py

--- pommerman/agents/__init__.py
+++ pommerman/agents/__init__.py
@@ -7,3 +7,6 @@ from .player_agent_blocking import PlayerAgentBlocking
 from .random_agent import RandomAgent
 from .simple_agent import SimpleAgent
 from .tensorforce_agent import TensorForceAgent
+
+from .a2c_agent import A2CAgent
+from .dqn_agent import DQNAgent
